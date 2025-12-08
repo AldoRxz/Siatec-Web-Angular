a7f2d0fc731a51301e073b7d3d306966fb6e4e66
Alkro <za20012307@zapopan.tecmm.edu.mx>
2025-12-07 19:06:40 -0600

    chore: scaffold Spartan design system

--- siatec-app/tailwind.config.js
+++ siatec-app/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  darkMode: ['class'],
+  content: ['./src/**/*.{html,ts}'],
+  presets: [require('@spartan-ng/brain/hlm-tailwind-preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)', 'Inter', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
